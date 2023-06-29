67fc4da34ec2118bce11b5773647755f6c1cdd08
jenish5571 <jenishhirapara89@gmail.com>
2023-06-29 10:00:16 +0530

    pr1

---
+++ pr1/1-1-1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Distance
+{
+	private:
+		int feet;
+		int inches;
+	public:
+		void set()
+		{
+			cout << "Enter feet: ";
+			cin  >> feet;
+			cout << "Enter inches: ";
+			cin  >> inches;
+		}
+		void get()
+		{
+			cout <<" feet  = "<<feet << endl;
+			cout <<" inches= "<<inches<<endl;
+		}
+		void add(Distance k1, Distance k2)
+		{
+			feet = k1.feet + k2.feet;
+			inches = k1.inches + k2.inches;
+			feet = feet + (inches / 12);
+			inches = inches % 12;
+		}
+};
+int main()
+{
+	Distance k1, k2, k3;
+	k1.set();
+	k2.set();
+	k3.add(k1, k2);
+	k3.get();
+
+}
+
+
+
+   
+
+    
+ 
+
+
+
+
+
+
+
---
+++ pr1/1-1-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Time 
+{
+	private:
+    	int seconds;
+    	int hh, mm, ss;
+
+	public:
+    	void getTime()
+    	{
+    		cout << "Enter time:" << endl;
+    
+    		cout << "Hours?   ";
+    		cin >> hh;
+   
+    		cout << "Minutes? ";
+    		cin >> mm;
+  
+    		cout << "Seconds? ";
+    		cin >> ss;
+		}
+    	void Seconds()
+		{
+   	 		seconds = hh * 3600 + mm * 60 + ss;
+		}
+    	void displayTime()
+		{	
+    		cout << " time = " << hh << ":"
+         	<< mm << ":"
+         	<< ss << endl;
+    		cout << "Time in total seconds: " << seconds;
+		}
+
+};
+
+int main()
+{
+    Time t; 
+
+    t.getTime();
+	t.Seconds();
+    t.displayTime();
+
+}
